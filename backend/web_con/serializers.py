2caff850365136d2a32c2fe3f8a2604059bab024
kimuson <dev.kimuson@gmail.com>
2020-11-12 07:53:02 +0900

    [update] RoomSerializer に参加メンバー一覧、タグ一覧を追加した

--- backend/web_con/serializers.py
+++ backend/web_con/serializers.py
@@ -1,9 +1,27 @@
 from rest_framework import serializers
+from typing import Any, Dict
 
-from .models import Room
+from accounts.serializers import UserSerializer
+from .models import Room, Tag
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ('pk', 'name',)
 
 
 class RoomSerializer(serializers.ModelSerializer):
+    members = serializers.SerializerMethodField()
+    tags = serializers.SerializerMethodField()
+
     class Meta:
         model = Room
-        fields = ('pk', 'title', 'description', 'start_datetime', 'end_datetime', 'is_possible_join',)
+        fields = ('pk', 'title', 'description', 'start_datetime',
+                  'end_datetime', 'is_possible_join', 'members', 'tags',)
+
+    def get_members(self, obj: Room) -> Dict[str, Any]:
+        return UserSerializer(instance=obj.members, many=True).data
+
+    def get_tags(self, obj: Room) -> Dict[str, Any]:
+        return TagSerializer(instance=obj.tags, many=True).data
