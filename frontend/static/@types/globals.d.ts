43657f141015be733c5e59f10fe656aef28fec5b
kimuson <dev.kimuson@gmail.com>
2020-11-12 11:52:05 +0900

    [update] Room, Room.join エンドポイント用の型定義を追加した

--- frontend/static/@types/globals.d.ts
+++ frontend/static/@types/globals.d.ts
@@ -1,3 +1,39 @@
+// DOM
 interface HTMLVideoElement {
   playsInline?: boolean
+}
+
+// Application
+interface User {
+  name: string
+  userId: string
+  peerId: string
+}
+
+// API
+interface UserSerializer {
+  pk: string
+  email: string
+}
+
+interface TagSerializer {
+  pk: string
+  name: string
+}
+
+interface RoomUserSerializer {
+  pk: string
+  user: UserSerializer
+  peer_id?: string
+}
+
+interface RoomSerializer {
+  pk: string
+  title: string
+  description: string
+  start_datetime: string
+  end_datetime: string
+  is_possible_join: string
+  room_members: RoomUserSerializer[]
+  tags: TagSerializer[]
 }
