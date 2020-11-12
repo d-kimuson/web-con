c36378cdabc3033d8f1777508e81c26b8be45e9d
kimuson <dev.kimuson@gmail.com>
2020-11-12 11:54:37 +0900

    [add] APIコール用に axios を追加して、エンドポイントをラップした関数を追加した

---
+++ frontend/static/scripts/api.ts
@@ -0,0 +1,62 @@
+import axios from "axios"
+
+export const api = axios.create({
+  baseURL: `/api/`,
+  timeout: 1000,
+  withCredentials: true,
+  xsrfCookieName: `csrftoken`,
+  xsrfHeaderName: `X-CSRFTOKEN`,
+})
+
+// API Endpoint Definition
+interface JoinRoomResponse {
+  message: string
+  user: UserSerializer
+  room: RoomSerializer
+}
+
+export const joinRoom = async (
+  roomId: string,
+  peerId: string,
+): Promise<JoinRoomResponse | undefined> => {
+  try {
+    const response: {
+      status: number
+      data: JoinRoomResponse
+    } = await api.post(`/rooms/${roomId}/join/`, {
+      peerId,
+    })
+
+    return response.data
+  } catch (error) {
+    const { status, statusText } = error.response
+
+    console.log(`ERROR(${status}): ${statusText}`)
+  }
+}
+
+export const roomList = async (): Promise<RoomSerializer[] | undefined> => {
+  try {
+    const response: RoomSerializer[] = await api.get(`/rooms/`)
+
+    return response
+  } catch (error) {
+    const { status, statusText } = error.response
+
+    console.log(`ERROR(${status}): ${statusText}`)
+  }
+}
+
+export const roomDetail = async (
+  roomId: string,
+): Promise<RoomSerializer | undefined> => {
+  try {
+    const response: RoomSerializer = await api.get(`/rooms/${roomId}/`)
+
+    return response
+  } catch (error) {
+    const { status, statusText } = error.response
+
+    console.log(`ERROR(${status}): ${statusText}`)
+  }
+}
