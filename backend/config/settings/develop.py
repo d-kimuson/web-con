34990c457af0ce2c603a8d6104e4446c85b98fce
kimuson <dev.kimuson@gmail.com>
2020-11-27 15:10:29 +0900

    [update] 開発環境に debug_toolbar を追加しました

--- backend/config/settings/develop.py
+++ backend/config/settings/develop.py
@@ -1,3 +1,5 @@
+import logging
+
 from .base import *
 
 DEBUG = True
@@ -10,10 +12,55 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS += [
     'django_extensions',
     'drf_spectacular',
+    'nplusone.ext.django',
+    'debug_toolbar',
 ]
+
+MIDDLEWARE += [
+    'nplusone.ext.django.NPlusOneMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+]
+
+DEBUG_TOOLBAR_CONFIG = {
+    "SHOW_TOOLBAR_CALLBACK": lambda _: True
+}
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+# N + 1クエリの自動検出ログ
+NPLUSONE_LOGGER = logging.getLogger('nplusone')
+NPLUSONE_LOG_LEVEL = logging.WARN
+
+LOGGING = {
+    'version': 1,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'nplusone': {
+            'handlers': ['console'],
+            'level': 'WARN',
+        },
+    },
 }
 
 DATABASES = {
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,3 +23,10 @@ urlpatterns = [
     path('', include('web_con.urls')),
     path('api/', include('web_con.urls_api')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar  # type: ignore
+
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
